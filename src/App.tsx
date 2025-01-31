00b3b7601055bbb07e970393dee2f33bf507b2e3
Tiago FM <tmarques@broadvoice.com>
2025-01-31 11:17:40 +0000

    App.tsx final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+//@ts-expect-error we know that we are using jsx
+import ContactCard from './ContactCard.jsx'; // <---- Importing the component we created previous
+
 function App() {
   return (
     <main className='container'>
       <h1>Contacts Manager</h1>
+      <ContactCard /> {/* <---- Now e can use the component */}
+      {/* Also, Hi👋 I'm a JSX comment! */}
     </main>
   );
 }
