9591575adfc05681bfadb0be37286fd930afe3e3
Tiago FM <tmarques@broadvoice.com>
2025-02-13 17:49:57 +0000

    step3: get data from api

--- docs/SESSION4.md
+++ docs/SESSION4.md
@@ -118,6 +118,46 @@ function App() {
 export default App;
 ```
 
+- Now, instead of hardcoded values, let's get them from an api:
+```jsx
+// App.tsx
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import {useEffect, useState} from 'react';
+import ContactCard from './ContactCard.jsx'; // <---- Importing the component we created previous
+
+function App() {
+  {/* we define the setter function */}
+  const [contacts, setContacts] = useState(null);
+
+  // Use useEffect to load the data on component mount
+  useEffect(() => {
+    const fetchContacts = async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
+      setContacts(data);
+    };
+
+    fetchContacts();
+  }, []); // Empty dependency array ensures this runs only once after the initial render
+
+  return (
+    <main className='container'>
+      <h1>Contacts Manager</h1>
+
+      {contacts?.map((contact, index) => (  // Nullish coalescing operator to prevent errors when contacts is null: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+        <ContactCard
+          key={index} /* don't forget the key, so react properly keeps track of mutation that require rerender */
+          name={contact.name}
+          email={contact.email}
+          phone={contact.phone}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default App;
+```
 
 ---
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 // App.tsx
-import {useState} from 'react';
-//@ts-expect-error we know that we are using jsx
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import {useEffect, useState} from 'react';
 import ContactCard from './ContactCard.jsx'; // <---- Importing the component we created previous
 
 function App() {
-  const [contacts] = useState([
-    {
-      name: 'John Cena',
-      email: 'jcena@wwe.com',
-      phone: '800-NO-SEE-ME',
-    },
-    {
-      name: 'Chuck Norries',
-      email: 'outlook@com.chucknorries',
-      phone: '555-TX-RANGER',
-    },
-    {
-      name: 'András István Arató',
-      email: 'harold@gmail.com',
-      phone: '800-HIDE-PAIN',
-    },
-  ]);
+  {/* we define the setter function */}
+  const [contacts, setContacts] = useState(null);
+
+  // Use useEffect to load the data on component mount
+  useEffect(() => {
+    const fetchContacts = async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
+      setContacts(data);
+    };
+
+    fetchContacts();
+  }, []); // Empty dependency array ensures this runs only once after the initial render
 
   return (
     <main className='container'>
       <h1>Contacts Manager</h1>
 
-      {contacts.map((contact, index) => (
+      {contacts?.map((contact, index) => (  // Nullish coalescing operator to prevent errors when contacts is null: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
         <ContactCard
           key={index} /* don't forget the key, so react properly keeps track of mutation that require rerender */
           name={contact.name}
