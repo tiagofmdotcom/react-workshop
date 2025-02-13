2c07d10cd3dbb15a05867f8f6df9c67810d47732
Tiago FM <tmarques@broadvoice.com>
2025-02-13 17:46:41 +0000

    step1: render from object array

--- docs/SESSION4.md
+++ docs/SESSION4.md
@@ -16,6 +16,63 @@ Have **React Devtools** browser extension installed: [Chrome-based](https://chro
 
 **Steps:**
 
+- Map the contact info into an object array, using useState hook:
+```jsx
+// App.tsx
+import {useState} from 'react';
+//@ts-expect-error we know that we are using jsx
+import ContactCard from './ContactCard.jsx'; // <---- Importing the component we created previous
+
+function App() {
+  const [contacts] = useState([
+    {
+      name: 'John Cena',
+      email: 'jcena@wwe.com',
+      phone: '800-NO-SEE-ME',
+    },
+    {
+      name: 'Chuck Norries',
+      email: 'outlook@com.chucknorries',
+      phone: '555-TX-RANGER',
+    },
+    {
+      name: 'András István Arató',
+      email: 'harold@gmail.com',
+      phone: '800-HIDE-PAIN',
+    },
+  ]);
+
+  return (
+    <main className='container'>
+      <h1>Contacts Manager</h1>
+
+      {/* we pass data via the attributes (aka, props) of our component */}
+      <ContactCard
+        name={contacts[0].name}
+        email={contacts[0].email}
+        phone={contacts[0].phone}
+      />
+      {/* we can create multiple instances of the component with different props*/}
+      <ContactCard
+        name={contacts[1].name}
+        email={contacts[1].email}
+        phone={contacts[1].phone}
+      />
+      {/* we binding the state array items */}
+      <ContactCard
+        name={contacts[2].name}
+        email={contacts[2].email}
+        phone={contacts[2].phone}
+      />
+      {/* <---- Now e can use the component */}
+      {/* Also, Hi👋 I'm a JSX comment! */}
+    </main>
+  );
+}
+
+export default App;
+```
+
 
 ---
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
+// App.tsx
+import {useState} from 'react';
 //@ts-expect-error we know that we are using jsx
 import ContactCard from './ContactCard.jsx'; // <---- Importing the component we created previous
 
 function App() {
+  const [contacts] = useState([
+    {
+      name: 'John Cena',
+      email: 'jcena@wwe.com',
+      phone: '800-NO-SEE-ME',
+    },
+    {
+      name: 'Chuck Norries',
+      email: 'outlook@com.chucknorries',
+      phone: '555-TX-RANGER',
+    },
+    {
+      name: 'András István Arató',
+      email: 'harold@gmail.com',
+      phone: '800-HIDE-PAIN',
+    },
+  ]);
+
   return (
     <main className='container'>
       <h1>Contacts Manager</h1>
 
       {/* we pass data via the attributes (aka, props) of our component */}
       <ContactCard
-        name='John Cena'
-        email='jcena@wwe.com'
-        phone='800-NO-SEE-ME'
+        name={contacts[0].name}
+        email={contacts[0].email}
+        phone={contacts[0].phone}
       />
       {/* we can create multiple instances of the component with different props*/}
       <ContactCard
-        name='Chuck Norries'
-        email='outlook@com.chucknorries'
-        phone='555-TX-RANGER'
+        name={contacts[1].name}
+        email={contacts[1].email}
+        phone={contacts[1].phone}
       />
+      {/* we binding the state array items */}
       <ContactCard
-        name='András István Arató'
-        email='harold@gmail.com'
-        phone='800-HIDE-PAIN'
+        name={contacts[2].name}
+        email={contacts[2].email}
+        phone={contacts[2].phone}
       />
       {/* <---- Now e can use the component */}
       {/* Also, Hi👋 I'm a JSX comment! */}
