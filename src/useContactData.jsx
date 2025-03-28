64973fb4c168014c094dd9a33a9ab8e50fdb8bf3
Tiago FM <tmarques@broadvoice.com>
2025-03-28 11:04:50 +0000

    step 2 - adapt hook to use context

--- src/useContactData.jsx
+++ src/useContactData.jsx
@@ -1,26 +1,3 @@
-### **Session 7: Context API and Prop Drilling**
-
-**Pre-requirements:**
-Have `node.js` v18+ and `yarn` installed:
-
-- https://nodejs.org/en/download
-- https://classic.yarnpkg.com/lang/en/docs/install/
-- Check correct `yarn` installation by running `yarn --version`
-
-Have completed previous sessions [#1](SESSION1.md) [#2](SESSION2.md) [#3](SESSION3.md) [#4](SESSION4.md) [#5](SESSION5.md) [#6](SESSION6.md)
-
-Have **React Devtools** browser extension installed: [Chrome-based](https://chromewebstore.google.com/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi) | [Firefox](https://addons.mozilla.org/en-US/firefox/addon/react-devtools/)
-
----
-
-#### Practice
-
-Goal: Fix state sharing issues by using useContext
-
-**Steps:**
-- rename useContactData hook to accept jsx
-- Adapt the hook to useContext
-```jsx
 // useContactData.js
 import { createContext, useContext, useState, useEffect, useMemo } from 'react';
 
@@ -75,10 +52,3 @@ export function ContactProvider({ children }) {
 export function useContacts() {
   return useContext(ContactContext);
 }
-```
-
-
----
-
-# Final result:
-
